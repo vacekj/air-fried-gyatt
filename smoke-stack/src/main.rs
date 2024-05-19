761aec5d504f70f038291b5623e22c0da2fd50ae
Philippe Dumonet <philippe@dumo.net>
2024-05-19 09:55:14 +0200

    refactor

--- smoke-stack/src/main.rs
+++ smoke-stack/src/main.rs
@@ -1,8 +1,6 @@
-use acir::circuit::ExpressionOrMemory::Expression;
 use acir::circuit::Opcode;
 use acir::native_types::WitnessStack;
-use circuit_checker::test::prove_and_verify;
-use circuit_checker::{generate_trace_from_plonk_rows, BabyBear, PlonkBuilder};
+use circuit_checker::{generate_trace_from_plonk_rows, prove_and_verify, BabyBear, PlonkBuilder};
 use clap::Parser;
 use nargo::artifacts::program::ProgramArtifact;
 use serde_json;
